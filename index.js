11e127de8081a95f629f48c9adf9d8308ce330d2
jarpi <josep.arpi@gmail.com>
2016-12-17 11:25:13 +0100

    REST endpoint + driver init + navigate to

---
+++ index.js
@@ -0,0 +1,35 @@
+var webdriver = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+var express = require('express');
+var app = express();
+
+var driver = new webdriver.Builder()
+.forBrowser('chrome')
+.build();
+
+var url = 'http://play.flaix.webtv.flumotion.com/play/player?player=22&pod=15&backgroundColor=#/FFFFFF&poster=http://go.spreadthefox.net/300x250-white.png&endOfVideoOverlay=http://go.spreadthefox.net/300x250-white.png';
+/* setTimeout(function() {
+driver.get(url)
+.then(function(res) {return driver.getPageSource();})
+.then(function(res) {console.dir(res);})
+.catch(function(err) {console.log('ERR') & console.dir(err);});
+}, 1000 * 5);
+*/
+
+function goToUrl(url) {
+    return driver.get('http://' + url)
+    .then(function(loaded) { return driver.getPageSource(); });
+}
+
+app.get('/:url', function(req, res) {
+    console.log(req.params.url);
+    return goToUrl(req.params.url)
+           .then(function(pageSource) { return res.status(200).send(pageSource)})
+           .catch(function(err) { return res.status(500).send(err)});
+});
+
+app.listen(3000, function() {
+console.log('Express running');
+});
+
+
